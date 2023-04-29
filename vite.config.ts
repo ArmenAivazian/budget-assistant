7c648d7b6b060a08aa281b1b29ca2d49e1b79925
Armen <armen.aivazian@amo.tech>
2023-04-29 14:56:23 +0300

    path resolver

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      assets: "src/assets/*",
+      components: "src/components/*",
+      styles: "src/styles/*",
+      types: "src/types/*.types.ts",
+      src: "src/*",
+    },
+  },
+});
