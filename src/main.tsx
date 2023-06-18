d42657cd72b43ace8b4086b99f48c3fbc545ac96
Armen <armen.aivazian@amo.tech>
2023-06-18 17:43:20 +0300

    add firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { StrictMode } from "react";
 
+import { initializeApp } from "firebase/app";
 import ReactDOM from "react-dom/client";
 
 import "./styles/fonts.css";
@@ -8,8 +9,18 @@ import "./styles/inputs.css";
 
 import App from "./App.tsx";
 
+initializeApp({
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
